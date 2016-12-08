07933e78b1172d2ae169ad0f73ecaf87cee89466
Steve Burton <stevevburton@gmail.comgit config --global user.email stevevburton@gmail.comgit config --global user.email stevevburton@gmail.com>
2016-12-08 10:27:04 -0800

    add homemodule and cshtml files

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -15,9 +15,18 @@ namespace Review
 			Get["/add-new-restaurant"] = _ => {
 				return View["add-new-restaurant.cshtml"];
 			};
-			Get["add-new-restaurant"] = _ => 
-			{
-				return View["index.cshtml"];
+			Get["/add-new-cuisine"] = _ => {
+				return View["add-new-cuisine.cshtml"];
+			};
+			// Post["/added-restaurant"] = _ => {
+			// 	Restaurant newRestaurant = new Restaurant(Request.Form["new-restaurant"], Request.Form["new-description"]);
+			// 	newRestaurant.Save();
+			// 	return View["added-restaurant.cshtml"];
+			// };
+			Post["/added-cuisine"] = _ => {
+				Cuisine newCuisine = new Cuisine(Request.Form["new-cuisine"]);
+				newCuisine.Save();
+				return View["added-cuisine.cshtml"];
 			};
 		}
 	}
